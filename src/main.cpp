d25ae211f251ddb79dbb2579053f8c8befd95714
Emirhan Gerçeker <emirhan@scihook.org>
2026-07-13 02:04:08 +0300

    feat: change UART communication method for reduce pins used

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,28 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/interrupt.h>
-#include <avr/delay.h>
+#include <util/delay.h>
 #include "uart.h"
 
-#define CA1 PD6
-#define CA2 PD5
-#define BUFFER_LEN 1024
+#define TX_REQ PD2      // INT0
+#define RX_REQ PD3      // INT1
+#define TX_ACK PD4      // Output
+#define RX_NEW_DATA PD5 // Output
 
+#define BUFFER_LEN 1024
 volatile char rx_buffer[BUFFER_LEN];
 volatile int head = 0; 
 volatile int tail = 0; 
 
 void interrupt_init(void) 
 {
-    DDRD &= ~(1 << PD2);
-    PORTD |= (1 << PD2);
-    MCUCR |= (1 << ISC01);
-    MCUCR &= ~(1 << ISC00);
-    GICR |= (1 << INT0);
-    sei();
+    DDRD &= ~((1 << TX_REQ) | (1 << RX_REQ)); // Set as input
+    PORTD |= (1 << TX_REQ) | (1 << RX_REQ);   // Enable pull-ups
+    
+    MCUCR |= (1 << ISC11) | (1 << ISC01);     // Set falling edge trigger
+    MCUCR &= ~((1 << ISC10) | (1 << ISC00));
+    
+    GICR |= (1 << INT0) | (1 << INT1);        // Enable external interrupts
+    sei();                                    // Enable global interrupts
 }
 
 ISR(USART_RXC_vect)
@@ -33,49 +36,56 @@ ISR(USART_RXC_vect)
     }
 }
 
+// 6502 to ATmega transmission
 ISR(INT0_vect) 
 {
-    uint8_t data = PINA;
-    PORTD &= ~(1 << PD7);
-    _delay_us(2); // Kısa bir ACK
-    PORTD |= (1 << PD7);
+    uint8_t data = PINA;             // Read from data bus
     UART_transmit(data);
+    
+    PORTD &= ~(1 << TX_ACK);         // Set TX ACK low (Data received)
+    while (!(PIND & (1 << TX_REQ))); // Wait until 6502 releases TX REQ
+    PORTD |= (1 << TX_ACK);          // Set TX ACK high (Ready)
 }
 
+// ATmega to 6502 transmission
+ISR(INT1_vect)  
+{
+    if (head != tail) 
+    {
+        DDRA = 0xFF;                 // Set data bus as output
+        PORTA = rx_buffer[tail];     // Put data on bus
+        tail = (tail + 1) % BUFFER_LEN;
+        
+        PORTD |= (1 << RX_NEW_DATA); // Set high (Data is ready on bus)
+        while (!(PIND & (1 << RX_REQ))); // Wait until 6502 releases RX REQ
+        
+        DDRA = 0x00;                 // Set data bus back to input
+        PORTA = 0x00;
+    }
+}
 
 int main(void) 
 {
-    DDRA = 0x00; 
-    DDRC = 0xFF; 
+    DDRA = 0x00;                     // Set data bus as input initially
     PORTA = 0x00;
-    PORTC = 0x00; 
-    
-    DDRD |= (1 << PD7); 
 
-    DDRD |= (1 << CA1); 
-    DDRD &= ~(1 << CA2); 
-
-    PORTD |= (1 << CA1);
-    PORTD &= ~(1 << CA2);
-    PORTD |= (1 << PD7);
+    DDRD |= (1 << TX_ACK) | (1 << RX_NEW_DATA); // Set control pins as output
+    PORTD |= (1 << TX_ACK);                     // Default HIGH
+    PORTD |= (1 << RX_NEW_DATA);                // Default HIGH (No data)
 
     UART_init(9600);
     interrupt_init();
 
     while (1) 
     {
-        if (head != tail)
+        if (head != tail) 
         {
-            PORTC = rx_buffer[tail];
-            _delay_us(1);
-            PORTD &= ~(1 << CA1);
-            _delay_us(2);  
-            PORTD |= (1 << CA1);
-            while (!(PIND & (1 << CA2)));
-            tail = (tail + 1 ) % BUFFER_LEN;
+            PORTD &= ~(1 << RX_NEW_DATA); // Data available (Active-Low)
+        }
+        else 
+        {
+            PORTD |= (1 << RX_NEW_DATA);  // Buffer empty
         }
     }
-    
     return 0;
 }
-
